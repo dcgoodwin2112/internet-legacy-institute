71c1bb531e92cca747126bb8f8fa6b79838a8099
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2021-01-14 19:34:37 -0500

    favicon update

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `internet-legacy-institute`,
+        short_name: `ili`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
